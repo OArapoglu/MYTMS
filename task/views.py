19e65cf1d0130c575d9be18f88d4fe810ceb6e4b
OArapoglu <98691749+OArapoglu@users.noreply.github.com>
2024-09-02 22:10:55 +0300

    Update views.py

--- task/views.py
+++ task/views.py
@@ -1,19 +1,12 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
-    @method_decorathor(cache_page(60*15))
-    def week_tasks(self, request, year, week):
-        tasks = Task.objects.filter(
-
-        )
 
 
 
