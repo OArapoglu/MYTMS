39e89ee376fca266a96794bdb75fc6c264de22ae
OArapoglu <98691749+OArapoglu@users.noreply.github.com>
2024-09-02 22:17:52 +0300

    pagination is added.

--- task/views.py
+++ task/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -6,3 +9,15 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    @method_decorator(cache_page(60 * 15))
+    def week_tasks(self, request, year, week):
+        tasks = Task.objects.filter(created_at__year=year, created_at__week=week)
+
+        page = self.paginate_queryset(tasks)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
